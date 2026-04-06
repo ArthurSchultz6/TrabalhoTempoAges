14876b5702729aa73f0738f7ba86e21fca7b0f62
LWBeck <l.beck02@edu.pucrs.br>
2026-04-06 16:34:57 -0300

    6 abril

--- script.js
+++ script.js
@@ -1,19 +1,63 @@
 
 //d2dd3ebe824843148eda1d70b6bba58d
+let lat = -30.033;
+let lon = -51.23;
 
+const local = document.getElementById("local");
+const cidades = document.getElementById("cidades");
 const horario = document.getElementById("horario"); // para saber o momento da API call, só para debugging
 const temp = document.getElementById("temp"); // graus atuais em celsius
 const sensacao = document.getElementById("sensacao"); // sensção térmica em célsius
 const temp_max = document.getElementById("temp_max"); // temperatura máxima do dia atual em celsius
 const temp_min = document.getElementById("temp_min"); // temperatura mínima do dia atual em celsius
 const nuvens = document.getElementById("nuvens"); // céu limpo, poucas nuvens, nublado, chovendo, etc
-const icone = document.getElementById("icone"); // icone de nublado/ensolarado etc
 const umidade = document.getElementById("umidade"); // umidade do ar em %
 const chuva = document.getElementById("chuva"); // precipitação em mm
 const vel_vento = document.getElementById("vel_vento"); // velocidade do vento em km/h
 const dir_vento = document.getElementById("dir_vento"); // direção do vento
 const nascer_do_sol = document.getElementById("nascer_do_sol"); // horário do nascer do sol do dia atual (HH:MM)
 const por_do_sol = document.getElementById("por_do_sol"); // horário do pôr do sol no dia atual (HH:MM)
+
+const forecast = document.getElementById("forecast");
+
+async function getCidades(){
+  try {
+    const response = await fetch('./city.list.min.json');
+    if (!response.ok) throw new Error('Network response was not ok');
+      
+    const listaCidades = await response.json(); // Parses JSON into a JS object
+    console.log(listaCidades);
+
+    listaCidades.forEach((i,index) => {
+      if (listaCidades[index].country == "BR"){
+        const novaCidade = document.createElement('option');
+        novaCidade.text = listaCidades[index].name;
+        novaCidade.value = listaCidades[index];
+        local.appendChild(novaCidade);
+        console.log(novaCidade.innerText+" - index: "+index);
+      }
+    });
+
+    local.customData = listaCidades[130613];
+    local.innerText = listaCidades[130613].name;
+    console.log("nome: "+local.customData.name)
+    
+  } catch (error) {
+    console.error('Error loading JSON:', error);
+  }
+
+}
+
+// Toggle menu on button click
+local.addEventListener('click', (e) => {
+  e.stopPropagation(); // Prevents immediate closing
+  cidades.style.display = cidades.style.display === 'block' ? 'none' : 'block';
+});
+
+// Close menu when clicking anywhere else on the page
+window.addEventListener('click', () => {
+  cidades.style.display = 'none';
+});
 
 async function getData() {
   const url =  "https://api.openweathermap.org/data/2.5/weather?units=metric&lang=pt_br&&lat=-30.033&lon=-51.23&appid=d2dd3ebe824843148eda1d70b6bba58d";
@@ -33,13 +77,43 @@ async function getData() {
     temp_max.innerText = obj.main.temp_max.toFixed(2) +"°C";
     temp_min.innerText = obj.main.temp_min.toFixed(2) +"°C";
     nuvens.innerText = obj.weather[0].description;
-    icone.innerHTML = get_icone(obj.weather[0].icon);
-    umidade.innerText = obj.main.humidity;
-    chuva.innerText = "precipitação placeholder";
+    umidade.innerText = obj.main.humidity+"%";
     vel_vento.innerText = (obj.wind.speed * 3.6).toFixed(2) +" km/h";
     dir_vento.innerText = deg_to_dir(obj.wind.deg);
     nascer_do_sol.innerText = sec_to_time(obj.sys.sunrise);
     por_do_sol.innerText = sec_to_time(obj.sys.sunset);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+async function getForecast() {
+  const url = "http://api.openweathermap.org/data/2.5//forecast?appid=d2dd3ebe824843148eda1d70b6bba58d&lat="+lat+"&lon="+lon+"&units=metric&lang=pt_br";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    obj = result;
+    console.log(result);
+    for (const entry of obj.list){
+      var row = forecast.insertRow();
+      var day = row.insertCell();
+      day.innerText = sec_to_date(entry.dt);
+      var time = row.insertCell();
+      time.innerText = sec_to_hour(entry.dt)+"h";
+      
+      var temp = row.insertCell();
+      temp.innerText = entry.main.temp+"°C";
+
+      var chuva = row.insertCell();
+      chuva.innerText = entry.weather[0].description;
+
+      var umidade = row.insertCell();
+      umidade.innerText = entry.main.humidity+"%";
+    }
   } catch (error) {
     console.error(error.message);
   }
@@ -80,10 +154,16 @@ function sec_to_time(sec){
   return time.getHours()+":"+time.getMinutes();
 }
 
-function get_icone(icon_id){
-  let icone = "icon placeholder";
-  return icone;
+function sec_to_hour(sec){
+  let time = new Date(sec*1000);
+  return time.getHours();
 }
+
+function sec_to_date(sec){
+  let time = new Date(sec*1000);
+  return time.getDay()+"/"+time.getMonth();
+}
+
 
 
 // https://api.openweathermap.org/data/2.5/forecast/daily?id=8467&cnt=16&appid=d2dd3ebe824843148eda1d70b6bba58d
